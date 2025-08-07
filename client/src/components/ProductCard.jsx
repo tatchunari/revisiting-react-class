759be821e51dcf5de9adcdc9e67605faad12c612
Orathai Saengsoemsap <mayomsaki@gmail.com>
2025-08-07 10:54:05 +0700

    fix: use destructuring on props

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -1,18 +1,19 @@
 import { ShoppingCartIcon } from "@heroicons/react/24/solid";
 
 function ProductCard(props) {
+  const { imgSrc, productName, productPrice} = props;
   return (
     <div className="bg-white p-4 shadow-md">
       <img
-        src={props.imgSrc}
-        alt={props.productName}
+        src={imgSrc}
+        alt={productName}
         className="w-full mb-2"
       />
       <h3 className="text-lg font-semibold mb-1">
-        {props.productName}
+        {productName}
       </h3>
       <div className="flex justify-between items-center">
-        <p className="text-gray-600">{props.productPrice}</p>
+        <p className="text-gray-600">{productPrice}</p>
         <button className="mt-2 border border-blue-500 text-blue-500 hover:bg-blue-500 hover:text-white py-2 px-4 rounded flex items-center">
           <ShoppingCartIcon className="h-4 w-4 mr-2" />
           Add to Cart
